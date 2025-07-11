995dcbed54ecbac0352d48bd8a627e51d690b350
Olusegun Gbokoyi <gbokoyiolusegun@gmail.com>
2025-07-11 03:31:32 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import react from 'react'
+
+
+function App() {
+
+
+  return (
+    <>
+     <h1 className='text-2xl underline text-center font-light text-green-600 mt-[100px]'>Hello world!</h1>
+    </>
+  )
+}
+
+export default App
